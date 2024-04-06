eaa2b0365921f9b58a35a7639adefa3f13fb182a
vonvernice <frandalozzo@gmail.com>
2024-04-06 09:47:10 -0300

    fix: remocao do contexto do spring em testes vazios

--- src/test/java/br/com/fight/stock/app/FightStockBackendApplicationTests.java
+++ src/test/java/br/com/fight/stock/app/FightStockBackendApplicationTests.java
@@ -3,10 +3,11 @@ package br.com.fight.stock.app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
+//TODO: criar testes unitarios
 class FightStockBackendApplicationTests {
 
-	@Test
+@Test
 	void contextLoads() {
 	}
 
