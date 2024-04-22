b9c862aef9823c7b290aa9453b36db81cd8205f3
vonvernice <frandalozzo@gmail.com>
2024-04-21 21:47:33 -0300

    chore: adicionando capacidade de capturar extensao do arquivo e nome e novos campos de atualizacao e criacao da entidade

--- src/main/java/br/com/fight/stock/app/domain/Image.java
+++ src/main/java/br/com/fight/stock/app/domain/Image.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
@@ -12,6 +14,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.Serializable;
+import java.time.Instant;
 import java.util.Base64;
 
 @Getter
@@ -29,21 +32,30 @@ public class Image implements Serializable {
     private Integer width;
     private String data;
     private String extension;
+    @CreationTimestamp
+    private Instant createdOn;
+    @UpdateTimestamp
+    private Instant lastUpdatedOn;
 
-    public Image(String name, Integer height, Integer width, String data) {
+    public Image(String name, Integer height, Integer width, String data, String extension) {
         this.name = name;
         this.height = height;
         this.width = width;
         this.data = data;
+        this.extension = extension;
     }
 
     public static Image createImage(MultipartFile file) throws IOException {
         ByteArrayInputStream bis = new ByteArrayInputStream(file.getBytes());
         BufferedImage imageBuffered = ImageIO.read(bis);
-        return new Image(file.getOriginalFilename(),
+        String fileName = file.getOriginalFilename();
+        int lastDotIndex = fileName.lastIndexOf(".");
+        String name = fileName.substring(0, lastDotIndex);
+        String extension = fileName.substring(lastDotIndex + 1);
+        return new Image(name,
                 imageBuffered.getHeight(),
                 imageBuffered.getWidth(),
                 Base64.getEncoder()
-                        .encodeToString(file.getBytes()));
+                        .encodeToString(file.getBytes()), extension);
     }
 }
