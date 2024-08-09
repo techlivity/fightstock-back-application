5f4c22a5b12aaa26ad58c05b01d0920cc4c1e2d2
tech <techlivity@gmail.com>
2024-08-09 17:24:17 -0300

    feat: adidicionando cross origin com app vercel

---
+++ src/main/java/br/com/fight/stock/app/configuration/CorsConfig.java
@@ -0,0 +1,25 @@
+package br.com.fight.stock.app.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(Environment environment) {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(environment.getProperty("FRONT_URL_VERCEL"))
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
